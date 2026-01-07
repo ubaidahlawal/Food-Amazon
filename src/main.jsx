150b1d10412a9f5698612ea59e1bf1fc3eca5860
ubaidahlawal <lawalubaidah2005@gmail.com>
2026-01-07 01:29:07 +0100

    Final Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './styles/variables.css';
+import './styles/Variables.css';
 import './App.css';
 import { CartProvider } from './context/CartContext.jsx';
 
